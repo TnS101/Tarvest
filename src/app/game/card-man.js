fd67a4f6aded640a7d77f160f713860c96645993
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-02 14:33:34 +0300

    Add Business Logic

--- src/app/game/card-man.js
+++ src/app/game/card-man.js
@@ -6,48 +6,77 @@ const CardInventoryModel = models.getModel('card-inventory');
 const UserModel = models.getModel('user');
 
 async function exe(action, cardId, userId) {
-    let result = null;
 
     const user = await modMan.getMod({ _id: userId }, UserModel);
     const card = await modMan.getMod({ _id: cardId }, CardModel);
     const inventory = await modMan.getMod({ userId: userId, cardId: cardId }, CardInventoryModel);
 
     if (action === 'loot') {
-        if (!inventory) {
-            inventory = new CardInventoryModel({ userId: userId, cardId: cardId, count: 1 });
-            inventory.save();
-        } else {
-            inventory.count--;
-            inventory.save();
-        }
-        user.inventorySpace--;
-        user.save();
+        inventoryIncrement(inventory, user._id, card._id, 1);
+
     } else if (action === 'disolve') {
-        inventorySubstract(1);
+        inventorySubstract(inventory, 1);
         for (const key in user) {
             if (user.hasOwnProperty(card.onDisolve.rewardType)) {
                 user[key] += card.onDisolve.amount;
             }
         }
-    } else if (action === 'craft') {
 
+    } else if (action === 'craft') {
+        card.onCraft.ingredients.reduce(function(acc, curr) {
+            const ingredient = await modMan.getMod({ userId: user._id, cardId: curr.id }, CardInventoryModel);
+
+            if (ingredient === undefined) {
+                invalid();
+            }
+            if (user[card.onCraft.cost.type] < card.onCraft.cost.amount) {
+                invalid();
+            }
+            inventorySubstract(ingredient, curr.amount);
+        }, 0);
+
+        user[card.onCraft.cost.type] -= card.onCraft.cost.amount;
+        user.xp += await modMan.getMod({ _id: card.onCraft.rewardId }, CardModel).xp;
+
+        if (user.xp > user.xpCap) {
+            import { levelUp } from '../identity/user-man.js'
+            await levelUp(user, modMan);
+        }
+
+        const craftedItemInventory = await modMan.getMod({ userId: user._id, cardId: card.onCraft.rewardId }, CardInventoryModel);
+        inventoryIncrement(craftedItemInventory, user._id, card.onCraft.rewardId, 1);
     }
 
-    function inventorySubstract(count) {
+    function inventoryIncrement(inventory, userId, cardId, count) {
+        if (!inventory) {
+            inventory = new CardInventoryModel({ userId: userId, cardId: cardId, count: count });
+            inventory.save();
+        } else {
+            inventory.count--;
+            inventory.save();
+        }
+
+        user.inventorySpace -= count;
+        user.save();
+    }
+
+    function inventorySubstract(inventory, count) {
         if (inventory.count > count) {
             inventory.count -= count;
             inventory.save();
         } else if (inventory.count == count) {
             CardInventoryModel.findOneAndDelete({ userId: user.Id, cardId: card.Id });
         } else {
-            throw new Error('Invalid Action!');
+            invalid();
         }
 
         user.inventorySpace += count;
         user.save();
     }
 
-    return result;
+    function invalid() {
+        throw new Error('Invalid Action!');
+    }
 }
 
 module.exports = exe;
