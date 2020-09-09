8d14a8cc5aa5a1373a0d084bf1666b90ad569f80
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-09-09 22:21:29 +0300

    Fix Column Builder

--- client/src/utilities/column-builder.js
+++ client/src/utilities/column-builder.js
@@ -2,8 +2,8 @@ export default function exe (object){
     const result = [];
     for (const key in object) {
         if (object.hasOwnProperty(key)) {
-            if (typeof object[key] != 'object') {
-            result.push({Header: key, accessor: key, filterable: true});
+            if (typeof object[key] !== 'object' && key !== 'createdAt'&& key !== 'updatedAt' && key != '__v'){
+                result.push({Header: key, accessor: key});
             }
         }
     }
